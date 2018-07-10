83d214206cde6105771a35379e04ac7a4287821d
jwkangmacpro2 <jwkang10@gmail.com>
2018-07-10 23:09:03 +0900

    (feat) CMakeFile addtion

---
+++ CMakeFiles/pose_annotation_tool.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pose_annotation_tool.dir/pose_annotation_tool.cpp.o"
+  "pose_annotation_tool.pdb"
+  "pose_annotation_tool"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pose_annotation_tool.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
